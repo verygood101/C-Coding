d65c53d3dd8404c758828dc4af25fa916fd93d50
verygood101 <97860290+verygood101@users.noreply.github.com>
2022-05-31 17:11:17 +1000

    Average of three numbers
    Finds the average of three numbers lol.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double num1;
+double num2;
+double num3;
+
+Console.WriteLine("Input number 1: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input number 2: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input number 1: ");
+num3 = Convert.ToDouble(Console.ReadLine());
+
+double average = (num1 + num2 + num3)/3;
+Console.Write("The average is " + average);
+Console.ReadLine();
+
